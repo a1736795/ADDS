9db8d757ebbb3ab427a25aac0f3aa9ff20b9d30c
Hoang Khuat <a1736795@student.adelaide.edu.au>
2022-09-14 14:55:56 +0930

    test

--- numDup_head.cpp
+++ numDup_head.cpp
@@ -1,11 +1,20 @@
+#include <stdio.h>
+
 int numDup(int arr[], int start, int size, int element) 
 {
   int dup = 0;
-  while (i<size)
+  while (dup<size)
   {
     if (arr[i] == element) 
         dup++;
     i += 1;
   }
   return dup;
+}
+
+int main()
+{
+    numDup({1,2,3,4,5},1,5,3);
+    return 0;
+
 }
